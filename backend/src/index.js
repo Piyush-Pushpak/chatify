21e88183cd066454382624abd80f289bcf1226b5
Piyush-Pushpak <piyush592005@gmail.com>
2025-04-03 23:30:32 +0530

    Backend Setup

---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes)
+
+app.listen(3000, () => {
+    console.log("Port is listening on 3000");
+})
