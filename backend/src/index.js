86ab0c77dba3209db29a74a2c443c62d2bce6f89
Piyush-Pushpak <piyush592005@gmail.com>
2025-04-04 17:25:58 +0530

    DataBase Setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
+dotenv.config();
 
 app.use("/api/auth", authRoutes)
 
-app.listen(3000, () => {
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
     console.log("Port is listening on 3000");
+    connectDB();
 })
