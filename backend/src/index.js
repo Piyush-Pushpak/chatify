b7b39815433d9ab894279f14615d4cce7429d0aa
Piyush-Pushpak <piyush592005@gmail.com>
2025-04-09 00:01:26 +0530

    Implement Message Route and setup frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
 
 const PORT = process.env.PORT;
 
