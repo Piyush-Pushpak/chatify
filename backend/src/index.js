1a539ba6a41b65433113ffad143f265382ec0984
Piyush-Pushpak <piyush592005@gmail.com>
2025-05-03 10:46:53 +0530

    Debug error while deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path"
+import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,7 +12,10 @@ import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const __dirname = path.resolve();
+
+// Fix for __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,14 +24,14 @@ app.use(cors({
     credentials: true
 }));
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
@@ -35,4 +40,4 @@ const PORT = process.env.PORT;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
-})
+});
